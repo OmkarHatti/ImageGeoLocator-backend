302190e25d6bbbdc4b36b503d50f7f275a5fc642
OmkarHatti <omkarhatti23@gmail.com>
2026-05-11 18:05:52 +0530

    new backend3

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://image-geo-locator-frontend.vercel.app/"],
+    allow_origins=["https://image-geo-locator-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

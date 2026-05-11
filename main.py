c3da0bb3ce390fe8e6832e06e6cdbbc31729cef0
OmkarHatti <omkarhatti23@gmail.com>
2026-05-11 18:26:31 +0530

    new33

--- main.py
+++ main.py
@@ -18,41 +18,37 @@ app.add_middleware(
 )
 
 
-# Convert GPS coordinates
+# Convert GPS
 def convert_to_degrees(value):
-    def to_float(v):
-        try:
-            return float(v)
-        except:
-            return float(v.num) / float(v.den)
 
-    d = to_float(value[0])
-    m = to_float(value[1])
-    s = to_float(value[2])
+    d = float(value[0])
+    m = float(value[1])
+    s = float(value[2])
 
     return d + (m / 60.0) + (s / 3600.0)
 
 
-# Extract Metadata
+# Extract metadata
 def extract_metadata(image):
 
-    exif_data = image.getexif()
-
-    if not exif_data:
-        return {}
-
     metadata = {}
+
+    exif = image._getexif()
+
+    if not exif:
+        return metadata
+
     gps_info = {}
 
-    for tag_id, value in exif_data.items():
+    for tag, value in exif.items():
 
-        tag_name = ExifTags.TAGS.get(tag_id, tag_id)
+        tag_name = ExifTags.TAGS.get(tag, tag)
 
         # Date & Time
         if tag_name == "DateTime":
             metadata["date_time"] = value
 
-        # Camera Brand
+        # Camera Make
         if tag_name == "Make":
             metadata["camera_make"] = value
 
@@ -60,23 +56,24 @@ def extract_metadata(image):
         if tag_name == "Model":
             metadata["camera_model"] = value
 
-        # GPS Data
+        # GPS
         if tag_name == "GPSInfo":
 
-            # SAFETY CHECK
-            if isinstance(value, dict):
+            for key in value.keys():
 
-                for key, val in value.items():
-                    gps_tag = ExifTags.GPSTAGS.get(key, key)
-                    gps_info[gps_tag] = val
+                gps_tag = ExifTags.GPSTAGS.get(key, key)
 
-    # Coordinates
+                gps_info[gps_tag] = value[key]
+
+    # Latitude
     lat = gps_info.get("GPSLatitude")
     lat_ref = gps_info.get("GPSLatitudeRef")
 
+    # Longitude
     lon = gps_info.get("GPSLongitude")
     lon_ref = gps_info.get("GPSLongitudeRef")
 
+    # ONLY IF GPS EXISTS
     if lat and lon:
 
         lat = convert_to_degrees(lat)
@@ -97,14 +94,8 @@ def extract_metadata(image):
 # Upload API
 @app.post("/upload")
 async def upload_image(file: UploadFile = File(...)):
-    try:
 
-        # CHECK IMAGE TYPE
-        if not file.content_type.startswith("image/"):
-            return {
-                "status": "error",
-                "message": "Please upload a valid image"
-            }
+    try:
 
         contents = await file.read()
 
@@ -118,6 +109,7 @@ async def upload_image(file: UploadFile = File(...)):
         }
 
     except Exception as e:
+
         return {
             "status": "error",
             "message": str(e)
@@ -126,4 +118,4 @@ async def upload_image(file: UploadFile = File(...)):
 
 @app.get("/")
 def home():
-    return {"message": "Backend running successfully"}
+    return {"message": "Backend running"}
